06d834f8ccdf087e63bdc6761449c2894d3792de
MAhmut <mahmutlist@gmail.com>
2026-09-07 12:50:16 +0500

    Configure Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { fileURLToPath, URL } from "node:url";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cloudflare(),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
